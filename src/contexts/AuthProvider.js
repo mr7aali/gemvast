3af6de655ea6ae765240a7e1e7a8a45dc79193fd
mr7aali <mr7aali@gmail.com>
2022-11-08 13:28:29 +0600

    header added :

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
     return (
         <div>
-            
+            {children}
         </div>
     );
 };
