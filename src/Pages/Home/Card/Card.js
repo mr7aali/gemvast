8af8fac42a016b51448f4d93593a670f7697075e
mr7aali <mr7aali@gmail.com>
2022-11-09 14:40:43 +0600

    added photo provider

--- src/Pages/Home/Card/Card.js
+++ src/Pages/Home/Card/Card.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Card.css'
+import './Card.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const Card = ({ serv}) => {
-    const {title,_id,price,details,photo} = serv; 
+
+const Card = ({ serv }) => {
+    const { title, _id, price, details, photo } = serv;
     return (
+        <PhotoProvider>
         <div className='mx-auto singleCard'>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className='card-photo' src={photo} alt="Shoes" /></figure>
+
+
+                
+                    <PhotoView src={photo}>
+                        <figure><img className='card-photo' src={photo} alt="Shoes" /></figure>
+                    </PhotoView>
+                
+
+
+
+
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
                     <p>
 
                         {
                             details.length > 100 ?
-                            <p>{details.slice(0, 90) + "..."} <Link className='text-primary' >Read More</Link></p>
-                            :
-                            <p>{details}</p>
+                                <p>{details.slice(0, 90) + "..."} <Link className='text-primary' >Read More</Link></p>
+                                :
+                                <p>{details}</p>
                         }
-                    
+
                     </p>
                     <div className="card-actions justify-end">
                         <p className='text-xl mt-2 font-bold'> Cost {price}$</p>
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">View Details</button>
                     </div>
                 </div>
             </div>
         </div>
+        </PhotoProvider>
     );
 };
 
